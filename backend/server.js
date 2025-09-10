d226dd8d1508c1db0ef0050da4ff02d8f3fc1aaa
bakernajem <bakernajem@hotmail.com>
2025-09-10 12:17:25 +0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,2 @@
-const express = require("express"); const app = express(); app.listen(5000, () => console.log("Backend running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
